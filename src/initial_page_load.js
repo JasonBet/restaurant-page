ebe611530f4208c4493679a2914ede32ea18a5a5
Jason <jasonbetsargon@gmail.com>
2024-11-28 15:51:31 -0800

    Correct module to export a container that will contain multiple elements

--- src/initial_page_load.js
+++ src/initial_page_load.js
@@ -1,10 +1,18 @@
 // Initial Page-load function
 export function home() {
+    const homeContainer = document.createElement("div");
+
     const name = document.createElement("h1");
     name.textContent = "CRUNCHY CRISPY CHICKEN";
     
+    const intro = document.createElement("p");
+    intro.textContent = "Come on o'er and try the CRUNCHIEST, CRISPIEST, CHICKENIEST Chicken " +
+        "you've ever had!";
+
+    homeContainer.appendChild(name);
+    homeContainer.appendChild(intro);
 
 
-
-    return name;
+    
+    return homeContainer;
 }
