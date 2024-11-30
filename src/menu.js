ee01a2f7a08b0238a2544f528b19fb923f5a1327
Jason <jasonbetsargon@gmail.com>
2024-11-30 15:55:14 -0800

    Improve styling/alignment and complete appetizers

--- src/menu.js
+++ src/menu.js
@@ -12,13 +12,14 @@ export function menu() {
     appsHead.textContent = "Appetizers";
     appsDiv.appendChild(appsHead);
 
+    // Appetizers
     const app1Div = document.createElement("div");
     const app1Name = document.createElement("h3");
     app1Name.textContent = "Shareable Wings";
     const app1Info = document.createElement("p");
     app1Info.textContent = "5 Drums, 5 Flats, AND 5 Boneless shareable wings & ranch. Chose up to 3 "
         + "flavors: Honey BBQ, Medium, Lemon Pepper, Garlic Parm";
-    const app1PriceAndImgDiv = document.createElement("div");
+    const app1PriceAndImgDiv = document.createElement("div"); // allow price and img to stay inline
     app1PriceAndImgDiv.classList.add("price-and-pic");
     const app1Price = document.createElement("p");
     app1Price.classList.add("price");
@@ -32,12 +33,35 @@ export function menu() {
     app1PriceAndImgDiv.appendChild(app1Price);
     app1PriceAndImgDiv.appendChild(app1PhotoPlaceholder);
 
-
     app1Div.appendChild(app1Name);
     app1Div.appendChild(app1Info);
     app1Div.appendChild(app1PriceAndImgDiv);
     
+
+    const app2Div = document.createElement("div");
+    const app2Name = document.createElement("h3");
+    app2Name.textContent = "Chicken Tacos";
+    const app2Info = document.createElement("p");
+    app2Info.textContent = "10 grilled chicken tacos with onion, tomato, and cheese. "
+        + "salsa: Pico De Gallo, Medium, Habanera Hot";
+    const app2PriceAndImgDiv = document.createElement("div"); // allow price and img to stay inline
+    app2PriceAndImgDiv.classList.add("price-and-pic");
+    const app2Price = document.createElement("p");
+    app2Price.classList.add("price");
+    app2Price.textContent = "$15";
+    const app2PhotoPlaceholder = document.createElement("div");
+    app2PhotoPlaceholder.classList.add("img-placeholder");
+    const app2PlaceholderText = document.createElement("p");
+    app2PlaceholderText.textContent = "IMG";
+    app2PhotoPlaceholder.appendChild(app2PlaceholderText);
     
+    app2PriceAndImgDiv.appendChild(app2Price);
+    app2PriceAndImgDiv.appendChild(app2PhotoPlaceholder);
+
+
+    app2Div.appendChild(app2Name);
+    app2Div.appendChild(app2Info);
+    app2Div.appendChild(app2PriceAndImgDiv);
     
 
 
@@ -47,6 +71,7 @@ export function menu() {
     menuContainer.appendChild(heading);
     menuContainer.appendChild(appsDiv);
     menuContainer.appendChild(app1Div);
+    menuContainer.appendChild(app2Div);
 
     return menuContainer;
 }
